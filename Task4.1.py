fceab75a348feb21f799aa5a8eaee27f62acfcf4
Vladimir <dv.quiz@yandex.ru>
2023-10-01 15:43:41 +0300

    U4_T2

--- Task4.1.py
+++ Task4.1.py
@@ -1,10 +1,38 @@
-# Напишите функцию для транспонирования матрицы
+# Напишите функцию принимающую на вход только ключевые параметры и возвращающую словарь,
+# где ключ — значение переданного аргумента, а значение — имя аргумента.
+# Если ключ не хешируем, используйте его строковое представление.
 
-Matrix = [[1, 2], [3, 4], [5, 6, 7]]
 
-transonated_matrix = [[0 for i in range(len(Matrix))] for j in range(len(Matrix[0]))]
-for j in range(len(Matrix)):
-    for i in range(len(Matrix[0])):
-        transonated_matrix[i][j] = Matrix[j][i]
-print(Matrix)
-print(transonated_matrix)
+values = input('Введите ключевые параметры в виде "значение"/"значение"/...:(Значение вводите без "") ').split('/')
+result = {}
+
+for i in values:
+    try:
+        result[int(i)] = int
+        continue
+    except:
+        pass
+    try:
+        result[float(i)] = float
+        continue
+    except:
+        pass
+    if i == 'None':
+        result[None] = None
+        continue
+    if '[' and ']' in i:
+        result[i] = list
+        continue
+    if '{' and '}' in i:
+        result[i] = set
+        continue
+    if '(' and ')' in i:
+        result[i] = tuple
+        continue
+    if i == 'True' or i == 'False':
+        result[i] = bool
+        continue
+    else:
+        result[i] = type(i)
+
+print(result)
